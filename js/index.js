fcdd333c9340a51852700e063af894e04d50e5f1
mpullis <mroeblin@Megs-Mac.local>
2016-11-10 21:46:51 -0500

    animations working !
    rollover and animations working - hooray!

--- js/index.js
+++ js/index.js
@@ -12,7 +12,47 @@ $("li.smallnav").on("click",function(){
  	//set the menu item to selected	
 });
 
-$(document).ready(function(){ 
-	$(".inner-wrapper::after").hide();
-	console.log("ready");
-}) 
+var boxes = ["box1","box2","box3","box4","box5","box6","box7","box8","box9"];
+
+var boxNum = 1;
+
+$(".box").hover(
+
+	function(){
+		id = $(this).attr("id");
+ 	    console.log(id);
+		console.log("on");
+		$(this).find(".overlay").animate({
+			opacity: 1
+		});
+		//$("#box1.span.overlay").fadeTo("slow",1);
+		// animate opacity to 1
+	},
+	function(){
+		console.log("off");
+		$(this).find(".overlay").animate({
+			opacity: 0
+		});
+		// animate opacity to 0
+	}
+);
+
+//when the document is ready animate the margin-top back to 0
+$( document ).ready(function() {
+	for (var i=0;i<9;i++) {
+	  slideBoxUp(i);
+	}
+});
+
+function slideBoxUp (boxIndex) {
+	console.log(boxIndex);
+	setTimeout(function(){
+	    $("#"+boxes[boxIndex]).animate({
+	    	"margin-top":0,
+	    	"opacity": 1
+	    },500, "swing");
+  },(200*boxIndex));
+};
+
+
+
